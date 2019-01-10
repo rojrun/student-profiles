f798457544152c9265adbc2e4dee36c47042d66b
rojrun <runroj@gmail.com>
2019-01-10 15:46:47 -0800

    Completed add item functionality

--- src/components/add_item/add_item.js
+++ src/components/add_item/add_item.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
-import {Field, reduxForm} from 'redux-form';
-import NavButton from './nav_button';
+import NavButton from '../nav_button';
+import AddItemForm from './add_item_form';
 
 class AddItem extends Component {
     render(){
@@ -8,11 +8,10 @@ class AddItem extends Component {
             <div>
                 <h1 className="center">Add Item</h1>
                 <NavButton color="black white-text" to="/">Back to List</NavButton>
+                <AddItemForm />
             </div>
         );
     }
 }
 
-export default reduxForm({
-    form: 'add-item'
-})(AddItem);
+export default AddItem;
