cf9a210558be52b581b2e8b21028988ead4b7518
rojrun <runroj@gmail.com>
2019-01-10 16:58:02 -0800

    Made middleware components

---
+++ src/middleware/promise.js
@@ -0,0 +1,24 @@
+export default (store) => (next) => (action) => {
+
+    if (!action.payload || !action.payload.then){
+        return next(action);
+    }
+
+    action.payload.then((resp) => {
+        const newAction = {
+            ...action,
+            payload: resp
+        }
+
+        store.dispatch(newAction);
+    });
+    return action.payload;
+}
+
+// export default function(store){
+//     return function(next){
+//         return function(action){
+//          //Code goes here
+//         }
+//     }
+// }
