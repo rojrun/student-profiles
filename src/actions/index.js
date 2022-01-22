5663bab5abcb80498bd94904454481b2f968eef6
rojrun <runroj@gmail.com>
2022-01-21 16:27:39 -0800

    added validate to onSubmit

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,10 +5,9 @@ import config from '../config';
 const {BASE_URL} = config.api;
 
 export function getAllStudents() {
-    const resp = axios.get(BASE_URL);
     return {
         type: types.GET_LIST_OF_ALL_STUDENTS,
-        payload: resp
+        payload: axios.get(BASE_URL)
     }
 }
 
@@ -20,18 +19,16 @@ export function addRef(ref) {
 }
 
 export function addNameFilterToState(input) {
-    const filter = input.target.value;
     return {
         type: types.ADD_NAME_FILTER_TO_STATE,
-        payload: filter
+        payload: input.target.value
     }
 }
 
 export function addTagFilterToState(input) {
-    const filter = input.target.value;
     return {
         type: types.ADD_TAG_FILTER_TO_STATE,
-        payload: filter
+        payload: input.target.value
     }
 }
 
@@ -41,13 +38,6 @@ export function searchByFilters() {
     }
 }
 
-// export function addTag(id, tag) {
-//     const idAndTag = new Array(id, tag);
-//     return {
-//         type: types.ADD_TAG,
-//         payload: idAndTag
-//     }
-// }
 export function addTag(tag) {
     return {
         type: types.ADD_TAG,
--- src/components/add_tag_form.js
+++ src/components/add_tag_form.js
@@ -5,73 +5,51 @@ import {addTag} from '../actions';
 import Input from './helpers/input.js';
 
 class AddTagForm extends Component {
-    // handleAddTag = (id, data) => {    
-    //     const node = this.props.parentDom[(data.findIndex(element => element.id === id))].getElementsByTagName("input")[0];
-    //     const value = node.value;
-    //     this.props.addTag(id, value);
-    //     this.props.reset();
-    // }
     handleAddTag = (values) => {
-        console.log("SubmissionError: ", SubmissionError);
-        this.props.addTag(values);
-        this.props.reset();
+        const {id, tagsList, addTag, reset} = this.props;
+        for (const key in values) {
+            if (values.hasOwnProperty(key)) {
+                const tag = values[key];
+                if (tagsList !== null) {
+                    const tagsListIndex = tagsList.findIndex(element => element.id === id);
+                    if (tagsListIndex > -1) { 
+                        // Checks if tag already exists
+                        if (tagsList[tagsListIndex].tags.includes(tag)) {
+                            reset();
+                            throw new SubmissionError({[key]: "'" + tag + "' already exists. Please enter a new tag."});
+                        } else {
+                            addTag(values);
+                            reset();
+                        }
+                    } else {
+                        addTag(values);
+                        reset();
+                    }
+                } else {
+                    addTag(values);
+                    reset();
+                }
+            }
+        }
     }
 
 	render() {
-        const {handleSubmit, id, data, tagExistsWarning, parentDom} = this.props;
+        const {handleSubmit, id, data, parentDom, clearSubmitErrors} = this.props;
         return (
             <div className="row">
-                {/* <form onSubmit={handleSubmit(() => this.handleAddTag(id, data))}> 
-                    <Field name={`addTag${id}`} label="Add a tag" size="col s10 m8" type="text" component={Input} liRefs={parentDom}
-                        id={`addTag${id}`} data={data} warning={tagExistsWarning}/>
-                </form>*/}
-
                 <form onSubmit={handleSubmit(this.handleAddTag)}>
                     <Field name={`addTag${id}`} label="Add a tag" size="col s10 m8" type="text" component={Input} liRefs={parentDom}
-                        id={`addTag${id}`} data={data} warning={tagExistsWarning}/>
+                        id={`addTag${id}`} data={data} onBlur={clearSubmitErrors}/>
                 </form>
             </div>
         );
     }
 }
 
-// Check if tag already exists
-function validate(values, props) {
-    // console.log("validate values: ", values);
-    // console.log("props: ", props);
-    const error = {};
-    for (const key in values) {
-        if (values.hasOwnProperty(key)) {
-            // const id = key.slice(6);
-            const id = props.id;
-            const tag = values[key];
-            // console.log("id: ", id);
-            // console.log("tag: ", tag);
-            if (props.tagsList !== null) {
-                const tagsListIndex = props.tagsList.findIndex(element => element.id === id);
-                if (tagsListIndex > -1) {
-                    if (props.tagsList[tagsListIndex].tags.includes(tag)) {
-                        error[key] = "'" + tag + "' already exists. Please enter a new tag.";
-                    }
-                }
-            }
-        }
-    }
-    // console.log('error: ', error);
-    return error;
-}
-
-function mapStateToProps(state) {
-    return {
-        tagExistsWarning: state.list.tagExistsWarning
-    }
-}
-
-AddTagForm = connect(mapStateToProps, {
+AddTagForm = connect(null, {
     addTag: addTag
 })(AddTagForm);
 
 export default reduxForm({
-    form: "add-tag-form",
-    validate: validate
+    form: "add-tag-form"
 })(AddTagForm);
